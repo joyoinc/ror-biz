9f7164931fd63dc80511b5cfd77e76e3ba5fc227
xxd <xxd@joyoedu.net>
2013-02-28 17:03:08 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biz::Application.config.secret_token = 'be777c86cc0a8d766eec4d4c5eba7684bdfc733bd3eae91763c1400b77e27f61470c2b3e5dabad21e82da153c25f8f456f7c62b0c7a1ec60b13094512bf60ed9'
